9207a6439aeaecda32f8fbcdcd83ebc2a977234c
Laurent NOËL <lnoel4289@gmail.com>
2024-11-06 20:39:31 +0100

    exo 8 ok

--- src/exercise/08.ts
+++ src/exercise/08.ts
@@ -9,7 +9,7 @@ init()
 // ✔️ Début de l'exercice
 // 🐶 Remplace le type `any` de `doSomeThing` qui est trop généraliste
 //  pour ques les fonctions `doSomeThing` ci-après fonctionnement
-let doSomeThing: any
+let doSomeThing: Function
 
 // ❌ NE PAS MODIFIER
 doSomeThing = () => {
@@ -25,9 +25,34 @@ doSomeThing('John')
 
 // 🐶 Implemente et spécifie les paramètres et types de la fonction `sum`
 // n'oublie pas de spécifier le type de retour
-function sum() {}
 
 // 🚀 N'oublie pas les bonus
+//
+// Bonus 1 & 2
+
+type CalculExpFunction = (base: number) => number
+function calculExp(cb: CalculExpFunction, base: number): number {
+  return cb(base)
+}
+
+function carre(base: number): number {
+  return base * base
+}
+
+displayText(`Le carré de ${5} est ${calculExp(carre, 5)}`)
+
+function cube(base: number): number {
+  return base * base * base
+}
+
+displayText(`Le cube de ${5} est ${calculExp(cube, 5)}`)
+
+// Bonus 2
+
+carre.description = 'Fonction au carré'
+cube.description = 'Fonction au cube'
+
+console.log(carre, cube)
 
 /*
 eslint
